826dc18322464718b798ffafa9a0a1d2af36a2f8
BFGNeil <79230094+BFGNeil@users.noreply.github.com>
2023-09-07 13:15:42 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/getting-started-guide"
+            to="/docs/getting-started/"
           >
             Getting Started Guide
           </Link>
